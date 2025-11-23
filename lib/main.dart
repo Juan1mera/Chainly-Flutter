19a33fea4572e0f1c220e365235ad246f7a833e6
Juan1Mera <98964201+Juan1mera@users.noreply.github.com>
2025-11-23 20:22:20 +0300

    fix: Cambio de logo

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Wallet App',
+      title: 'Chainly',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: AppColors.purple),
         useMaterial3: true,
